ad518635a5a7f719d32fa2712f9f5ce38c5b0170
MasteRminD6666 <rami-zaitoun99@hotmail.com>
2021-12-27 05:06:16 +0200

    adding login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,97 @@
-
-
+import Header from './components/Header';
+import React, { useState,useEffect} from 'react';
+import LoginForm from './components/Auth/RegistrationForm';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 function App() {
+
+  let [role, setRole] = useState('');
+  let [loggedIn, setloggedIn] = useState(false);
+  let [switcher, setSwitcher] = useState(false);
+ 
+
+
+  useEffect(() => {
+    setloggedIn(JSON.parse(window.localStorage.getItem('Token')))
+    console.log(loggedIn);
+  
+  }, [])
+
+  function switchBtn(e) {
+    setSwitcher(e)
+  }
+
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    let registerData = {
+      username: e.target.username.value,
+      email: e.target.email.value,
+      password: e.target.password.value,
+      role: role,
+    }
+    axios.post('http://localhost:3001/signup', registerData).then((result) => {
+      console.log(result.data);
+    }).catch(e => {
+      console.log(e.message);
+    })
+
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Your account created successfully',
+      showConfirmButton: false,
+      timer: 1500
+    })
+    switchBtn(false)
+  }
+  function handleLogin(e){
+    e.preventDefault();
+    let LoginData = {
+      username: e.target.username.value,
+      password: e.target.password.value,
+    
+    }
+    axios.post('http://localhost:3001/signin', LoginData).then((result) => {
+      const accessToken = result.data
+      console.log(result.data);
+      window.localStorage.setItem('Token', JSON.stringify(accessToken));
+    }).catch(e => {
+      console.log(e.message);
+    })
+    console.log(loggedIn);
+    if (loggedIn) {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Your account created successfully',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'username or password is incorrect',
+      })
+    }
+  }
+  function handleDropdownChange(e) {
+    setRole(e.target.value)
+  }
   return (
-    <h1>Welcome to react</h1>
+    <>
+      <LoginForm
+        handleSubmit={handleSubmit}
+        handleDropdownChange={handleDropdownChange}
+        handleLogin={handleLogin}
+        switchBtn ={switchBtn}
+        loggedIn = {loggedIn}
+        switcher= {switcher}
+
+      />
+
+    </>
   );
 }
 
